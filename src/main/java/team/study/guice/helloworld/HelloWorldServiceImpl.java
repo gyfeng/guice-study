ea1991a6c7be6a60b18839337e1805dfa31f0a6b
gyfeng <yuanfeng.guo@foxmail.com>
2016-12-21 14:56:24 +0800

    重构-使用日志组件打印输出！

--- src/main/java/team/study/guice/helloworld/HelloWorldServiceImpl.java
+++ src/main/java/team/study/guice/helloworld/HelloWorldServiceImpl.java
@@ -1,13 +1,18 @@
 package team.study.guice.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author GYFeng by 2016/12/21
  * @version 1.0
  */
 public class HelloWorldServiceImpl implements HelloWorldService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldServiceImpl.class);
+
     @Override
     public void output() {
-        System.out.println("hello world!guice!");
+        LOGGER.info("hi {},welcome to guice world!", "soldier");
     }
 }
